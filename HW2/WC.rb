b114ec3ccd7ac4d2603933417b26212bdfba63b3
Nat Welch <nat@natwelch.com>
2010-03-24 01:14:38 -0700

    Starting HW2

---
+++ HW2/WC.rb
@@ -0,0 +1,22 @@
+# Counts words
+
+h = Hash.new(0)
+ARGV.each { |file| 
+   File.open(file, "r").each { |line|
+      line.split.each {|word|
+         # Strip away punctuation at beg and end
+         word.gsub!(/^[(,$?!\"_\'{}@#|*%+\\=\-\^\<\/>\[\]&;:.)]*/, '')
+         word.gsub!(/[(,$?!\"_\'{}@#|*%+\\=\-\^\<\/>\[\]&;:.)]*$/, '')
+
+         if word.length > 1 
+            h[word] = h[word] + 1
+         end
+     }
+   }
+}
+
+# sort by value in descending order, then by Key in ascending order, and then
+# print it in this sorted order
+h.sort{|a,b| c = b[1] <=> a[1]; if c == 0; a <=> b; else c end }.each { |elem|
+  puts "\"#{elem[0]}\" has #{elem[1]} occurrences"
+}
