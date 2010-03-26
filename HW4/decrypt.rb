808bab7fdaf48d7924ed4d97482e27a461cf283f
Nat Welch <nat@natwelch.com>
2010-03-26 10:44:29 -0700

    An idea yo

--- HW4/decrypt.rb
+++ HW4/decrypt.rb
@@ -7,7 +7,7 @@ def countLetters file
    file.each { |line| line.downcase.split(//).each {|x| count[x.gsub(/\W/,'')] += 1} }
    count.delete("")
 
-   return count.sort {|a, b| b[1] <=> a[1] }.map { |k, v| k }
+   return count.sort {|a, b| b[1] <=> a[1] }.map { |k, v| k }.join
 end
 
 def replace str, from, to
@@ -15,19 +15,18 @@ def replace str, from, to
       raise Exception.new("encoding patterns do not match.")
    end
 
-      p from
-      p to
-
-   from.each_index {|i| 
-      str.gsub!(/#{from[i]}/i,to[i])
-   }
-
-   return str.capitalize
+   return str.split(//).map {|x|  y = to[from.index(x.downcase)] 
+      if x >= "A"
+         y.capitalize
+      else
+         y
+      end
+   }.join
 end
 
 ARGV.each { |file| 
    fc = File.open(file, "r").readlines.to_s
-   to = "etaoinshrdlcumwfgypbvkjxqz".split(//)
+   to = "etaoinshrdlcumwfgypbvkjxqz"
    from = countLetters fc # create array with characters sorted by frequency
    puts fc
    puts replace(fc, from, to)
