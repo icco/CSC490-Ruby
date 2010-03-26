5725369b9585d0814b0bc5c2a9f0123d40c277de
Nat Welch <nat@natwelch.com>
2010-03-25 23:56:38 -0700

    progress

--- HW4/decrypt.rb
+++ HW4/decrypt.rb
@@ -1,10 +1,36 @@
 #!/usr/bin/ruby1.8
-# Decrypts an stuff
+# Decrypts and stuff
+# @author Nathaniel "Nat" Welch
 
-# For each file
-ARGV.each { |file| 
-   File.open(file, "r").each { |line|
-      puts line
+def countLetters file
+   count = Hash.new(0)
+   file.each { |line| line.downcase.split(//).each {|x| count[x.gsub(/\W/,'')] += 1} }
+   count.delete("")
+
+   return count.sort {|a, b| b[1] <=> a[1] }.map { |k, v| k }
+end
+
+def replace str, from, to
+   if from.length != to.length
+      raise Exception.new("encoding patterns do not match.")
+   end
+
+      p from
+      p to
+
+   from.each_index {|i| 
+      str.gsub!(/#{from[i]}/i,to[i])
    }
+
+   return str.capitalize
+end
+
+ARGV.each { |file| 
+   fc = File.open(file, "r").readlines.to_s
+   to = "etaoinshrdlcumwfgypbvkjxqz".split(//)
+   from = countLetters fc # create array with characters sorted by frequency
+   puts fc
+   puts replace(fc, from, to)
+
 }
 
