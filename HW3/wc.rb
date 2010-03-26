1bd58e566f170ba0278c7e19a78692b792244f94
Nat Welch <nat@natwelch.com>
2010-03-25 20:01:22 -0700

    SOOOO CLOSE

--- HW3/wc.rb
+++ HW3/wc.rb
@@ -1,64 +1,66 @@
 # Counts words
 
 class CollString < String
-   @@lookup = Hash.new(0).merge({
-      'W' => 0, 'w' => 1, 'A' => 2, 'a' => 3, 'B' => 4, 'b' => 5, 'Q' => 6, 
-      'q' => 7, 'C' => 8, 'c' => 9, 'D' => 10, 'd' => 11, 'E' => 12, 'e' => 13,
-      'F' => 14, 'f' => 15, 'G' => 16, 'g' => 17, 'H' => 18, 'h' => 19, 
-      'I' => 20, 'i' => 21, 'J' => 22, 'j' => 23, 'K' => 24, 'k' => 25,
-      'L' => 26, 'l' => 27, 'M' => 28, 'm' => 29, 'N' => 30, 'n' => 31, 
-      'O' => 32, 'o' => 33, 'P' => 34, 'p' => 35, 'R' => 36, 'r' => 37,
-      'S' => 38, 's' => 39, 'T' => 40, 't' => 41, 'U' => 42, 'u' => 43,
-      'V' => 44, 'v' => 45, 'X' => 46, 'x' => 47, 'Y' => 48, 'y' => 49, 
-      'Z' => 50, 'z' => 51, '1' => 52, '2' => 53, '3' => 54, '4' => 55, 
-      '5' => 56, '6' => 57, '7' => 58, '8' => 59, '9' => 60, '0' => 61 
-   })
+   @@lookup = "_AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz1234567890"
 
    def <=>(other)
       a = self.to_s
       b = other.to_s
 
-      aa = a.split(//).map {|x| @@lookup[x]}
-      ba = b.split(//).map {|x| @@lookup[x]}
-
-      for i in 0...aa.length
-         if aa[i] == ba[i]
-            aa.delete_at(i)
-            ba.delete_at(i)
+      for i in 0...a.length
+         ai = a.length <= 0 ? -100 : @@lookup.index(a[0])
+         bi = b.length <= 0 ? -100 : @@lookup.index(b[0])
+         if ai == bi
+            a = a.reverse.chop.reverse
+            b = b.reverse.chop.reverse
          else
             break
          end
       end
 
-      aa <=> ba
+      ai <=> bi
    end
 end
 
-class Count < Integer
-   @lines = []
+class MyHash < Hash
+   def <=>(other)
+      a = self.keys.sort
+      b = other.keys.sort
+      a <=> b
+   end
 end
 
 # Init Hash
-h = Hash.new(Count.new(0))
+h = MyHash.new()
 
 # For each file
 ARGV.each { |file| 
+   linec = 0
    File.open(file, "r").each { |line|
+      linec += 1
       line.split(/\W/).each {|word|
+         key = CollString.new(word)
          if word.length > 0
-            key = CollString.new(word)
-            h[key] = h[word] + 1
-            h[key] = h[word].lines.push(line)
+            if !h.key? key
+               h[key] = MyHash.new()
+            end
+
+            if !h[key].key? file
+               h[key][file] = [];
+            end
+               
+            h[key][file].push(linec)
          end
       }
    }
 }
 
-p h.inspect
-
 # sort by value in descending order, then by Key in ascending order, and then
 # print it in this sorted order
-h.sort{|a,b| c = (b[1] <=> a[1]); c == 0 ? a <=> b : c}.each { |elem|
-   puts "\"#{elem[0]}\" has #{elem[1]} occurrences"
+h.sort.each { |name, files|
+   puts "For #{name}: "
+   files.each { |fname, lines|
+      puts "   (#{fname}) #{lines.uniq.join(", ")}" 
+   }
 }
 
